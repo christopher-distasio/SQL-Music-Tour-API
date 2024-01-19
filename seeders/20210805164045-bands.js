604a95e5f5f941f2f204fb47d8849f710ad722bb
Chris DiStasio <stahzz1970@gmail.com>
2024-01-19 11:39:09 -0600

    Updates to solve date format errors

--- seeders/20210805164045-bands.js
+++ seeders/20210805164045-bands.js
@@ -2,14 +2,15 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const currentYear = new Date().getFullYear();
     await queryInterface.bulkInsert('bands', [{
       name: 'The Useful Books',
       genre: 'Rock',
-      available_start_time: '11:00:00',
-      end_time: '23:00:00'
+      available_start_time: `${currentYear}-01-01 11:00:00`,  // Adjusted timestamp
+      end_time: `${currentYear}-01-01 23:00:00`  // Adjusted timestamp
     }])
   },
-
+  
   down: async (queryInterface, Sequelize) => {
     // note that this deletes ALL data from the bands table
     await queryInterface.bulkDelete('bands', null, {})
